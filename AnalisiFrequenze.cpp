a66cedd5376b201a6120ae1d1d9304a9daea5dd0
Manu-2001 <89068559+Manu-2001@users.noreply.github.com>
2022-04-24 02:17:06 +0200

    Add files via upload

--- AnalisiFrequenze.cpp
+++ AnalisiFrequenze.cpp
@@ -9,14 +9,17 @@
 #include "TROOT.h"
 #include "TStyle.h"
 
-// Dati: frequenza range ristretto 3.75 kHz - 4.75 kHz
+// Dati: frequenza range ristretto 3.75 Hz - 4.75 Hz
 // Sorgente: sinusoide di ampiezza 5V, sweep con salti di 10Hz
 // Campionamento: 300 KHz
-// CrossOver trovato: 4055.99 Hz ± /
+// CrossOver trovato: (4052.0 ± 0.9) Hz
 
-void SmallRange() {
+double eval(double);
+
+int SmallRange() {
   TGraph* Tweeter = new TGraph("tweeter.txt", "%lg%lg", "");
   TGraph* Woofer = new TGraph("woofer.txt", "%lg%lg", "");
+  TGraph* Rumore = new TGraph("rumore.txt", "%lg%lg", "");
 
   TF1* parabola = new TF1("parabola", "[0]*x*x + [1]*x + [2]", 3000., 5000.);
 
@@ -34,22 +37,51 @@ void SmallRange() {
   Woofer->Draw();
   FitWoofer->Draw("SAME");
 
-  TCanvas* Confronto = new TCanvas("Confronto", "Sovrapposizione fit");
+  TCanvas* Confronto = new TCanvas("Confronto", "Woofer fit");
   FitTweeter->Draw();
   FitWoofer->Draw("SAME");
 
   double const* Tpar = FitTweeter->GetParameters();
   double const* Wpar = FitWoofer->GetParameters();
+  double const* TparError = FitTweeter->GetParErrors();
+  double const* WparError = FitWoofer->GetParErrors();
 
   double const a = Tpar[0] - Wpar[0];
   double const b = Tpar[1] - Wpar[1];
   double const c = Tpar[2] - Wpar[2];
 
-  double const delta = b * b - 4 * a * c;
+  double const da = TparError[0] + WparError[0];
+  double const db = TparError[1] + WparError[1];
+  double const dc = TparError[2] + WparError[2];
 
-  if (delta >= 0. && a != 0.) {
-    double const x1 = -(b + sqrt(delta)) / (2 * a);
-    double const x2 = (-b + sqrt(delta)) / (2 * a);
-    std::cout << "\nZERI:\n  x1 = " << x1 << " Hz\tx2 = " << x2 << " Hz.\n";
+  double const delta = b * b - 4 * a * c;
+  double const ddelta = 4 * eval(c) * da + 2 * eval(b) * db + 4 * eval(a) * dc;
+
+  if (delta < 0.) {
+    return EXIT_FAILURE;
   }
+
+  double const x1 = (a == 0.) ? -c / a : (-b + sqrt(delta)) / (2 * a);
+  double const x2 = (a == 0.) ? -c / a : -(b + sqrt(delta)) / (2 * a);
+
+  double const dx1 =
+      (a == 0.) ? eval(1 / a) * dc + eval(c / (pow(a, 2))) * da
+                : eval(0.5 / a * (b / sqrt(delta) - 1)) * db +
+                      eval(1 / sqrt(delta)) * dc +
+                      eval(0.5 * b - 0.5 * sqrt(delta) - a * c / sqrt(delta)) *
+                          da / pow(a, 2);
+
+  double const dx2 =
+      (a == 0.) ? eval(1 / a) * dc + eval(c / (pow(a, 2))) * da
+                : eval(0.5 * b + 0.5 * sqrt(delta) + a * c / sqrt(delta)) * da /
+                          pow(a, 2) +
+                      0.5 * eval(1 / a + b / sqrt(delta) / a) * db +
+                      eval(1 / sqrt(delta)) * dc;
+
+  std::cout << "\nZERI:\n  x1 = (" << x1 << " ± " << dx1 << ") Hz\tx2 = (" << x2
+            << " ± " << dx2 << ") Hz.\n";
+
+  return 0;
 }
+
+double eval(double x) { return (x >= 0.) ? x : -x; }
