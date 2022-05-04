0984725939eb6a89cbb5df342ee9c0ef566c080b
Manu-2001 <89068559+Manu-2001@users.noreply.github.com>
2022-05-04 23:07:39 +0200

    funzione statistic
    Ho aggiunto una funzione per calcolare la media e la deviazione standard del rumore.

--- AnalisiFrequenze.cpp
+++ AnalisiFrequenze.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <fstream>
 #include <iostream>
 
 #include "TCanvas.h"
@@ -15,9 +16,22 @@
 // Analisi da fit parabolico:
 //  - CrossOver trovato: (4052.0 ± 0.9) Hz
 
-double eval(double);
+double eval(double);  // funzione valore assoluto
+void Statistic(double&, double&, double&,
+               double&);  // calcola media e deviazione standard
 
-int SmallRange() {
+int SmallRange(bool draw = false) {
+  double f{};
+  double fStd{};
+  double V{};
+  double VStd{};
+
+  Statistic(f, V, fStd, VStd);
+  std::cout << "\nAnalisi rumore per la fase:"
+            << "\n Frequenza: media: " << f << "\tDeviazione Standard: " << fStd
+            << ".\n Diff. di potenziale: media: " << V
+            << "\tDeviazione Standard: " << VStd << ".\n";
+
   TGraph* Tweeter = new TGraph("tweeter.txt", "%lg%lg", "");
   TGraph* Woofer = new TGraph("woofer.txt", "%lg%lg", "");
   TGraph* Rumore = new TGraph("rumore.txt", "%lg%lg", "");
@@ -27,20 +41,22 @@ int SmallRange() {
   Tweeter->Fit("parabola", "R");
   TF1* FitTweeter = Tweeter->GetFunction("parabola");
 
-  TCanvas* TResult = new TCanvas("TResult", "Tweeter fit");
-  Tweeter->Draw();
-  FitTweeter->Draw("SAME");
-
   Woofer->Fit("parabola", "R");
   TF1* FitWoofer = Woofer->GetFunction("parabola");
 
-  TCanvas* WResult = new TCanvas("WResult", "Woofer fit");
-  Woofer->Draw();
-  FitWoofer->Draw("SAME");
+  if (draw) {
+    TCanvas* TResult = new TCanvas("TResult", "Tweeter fit");
+    Tweeter->Draw();
+    FitTweeter->Draw("SAME");
 
-  TCanvas* Confronto = new TCanvas("Confronto", "Woofer fit");
-  FitTweeter->Draw();
-  FitWoofer->Draw("SAME");
+    TCanvas* WResult = new TCanvas("WResult", "Woofer fit");
+    Woofer->Draw();
+    FitWoofer->Draw("SAME");
+
+    TCanvas* Confronto = new TCanvas("Confronto", "Intersezione dei fit");
+    FitTweeter->Draw();
+    FitWoofer->Draw("SAME");
+  }
 
   double const* Tpar = FitTweeter->GetParameters();
   double const* Wpar = FitWoofer->GetParameters();
@@ -61,27 +77,79 @@ int SmallRange() {
     return EXIT_FAILURE;
   }
 
-  double const x1 = (a == 0.) ? -c / a : (-b + sqrt(delta)) / (2 * a);
-  double const x2 = (a == 0.) ? -c / a : -(b + sqrt(delta)) / (2 * a);
+  double const f1 = (a == 0.) ? -c / a : (-b + sqrt(delta)) / (2 * a);
+  double const f2 = (a == 0.) ? -c / a : -(b + sqrt(delta)) / (2 * a);
 
-  double const dx1 =
+  double const df1 =
       (a == 0.) ? eval(c / (pow(a, 2))) * da + eval(1 / a) * dc
                 : eval(0.5 * b - 0.5 * sqrt(delta) - a * c / sqrt(delta)) * da /
                           pow(a, 2) +
                       eval(0.5 / a * (b / sqrt(delta) - 1)) * db +
                       eval(1 / sqrt(delta)) * dc;
 
-  double const dx2 =
+  double const df2 =
       (a == 0.) ? eval(c / (pow(a, 2))) * da + eval(1 / a) * dc
                 : eval(0.5 * b + 0.5 * sqrt(delta) + a * c / sqrt(delta)) * da /
                           pow(a, 2) +
                       0.5 * eval(1 / a + b / sqrt(delta) / a) * db +
                       eval(1 / sqrt(delta)) * dc;
 
-  std::cout << "\nZERI:\n  x1 = (" << x1 << " ± " << dx1 << ") Hz\tx2 = (" << x2
-            << " ± " << dx2 << ") Hz.\n";
+  std::cout << "\nCrossOver:\n  f1 = (" << f1 << " ± " << df1 << ") Hz\tf2 = ("
+            << f2 << " ± " << df2 << ") Hz.\n";
 
   return 0;
 }
 
 double eval(double x) { return (x >= 0.) ? x : -x; }
+
+void Statistic(double& X, double& Y, double& XStd, double& Ystd) {
+  std::ifstream in;
+
+  double x{}, y{};
+  double dx2{}, dy2{};
+  int N{};
+
+  X = 0.;
+  Y = 0.;
+  XStd = 0.;
+  Ystd = 0.;
+
+  // Calcolo media e deviazione standard sulle x e sulle y
+  in.open("rumore.txt");
+
+  while (true) {
+    in >> x >> y;
+    if (!in.good()) {
+      break;
+    } else {
+      ++N;
+      X += x;
+      Y += y;
+    }
+  }
+
+  in.close();
+  
+  // medie
+  X = X / N;
+  Y = Y / N;
+
+  in.open("rumore.txt");
+
+  while (true) {
+    in >> x >> y;
+    if (!in.good()) {
+      break;
+    } else {
+      ++N;
+      dx2 += pow(x - X, 2);
+      dy2 += pow(y - Y, 2);
+    }
+  }
+
+  in.close();
+
+  // deviazioni standard
+  XStd = sqrt(dx2 / (N - 1));
+  Ystd = sqrt(dy2 / (N - 1));
+}
